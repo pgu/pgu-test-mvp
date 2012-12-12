fea56b23c3842392e837260b48933c4ca6fc0677
Pascal Guilcher <guilcher.p@sfeir.com>
2012-12-12 17:35:21 +0100

    ..

--- src/pgu/mvp/client/ui/left/ALeftViewImpl.java
+++ src/pgu/mvp/client/ui/left/ALeftViewImpl.java
@@ -3,8 +3,12 @@ package pgu.mvp.client.ui.left;
 import pgu.mvp.client.ui.ALeftView;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.LoadEvent;
+import com.google.gwt.event.dom.client.LoadHandler;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Frame;
 import com.google.gwt.user.client.ui.Widget;
 
 public class ALeftViewImpl extends Composite implements ALeftView {
@@ -14,10 +18,27 @@ public class ALeftViewImpl extends Composite implements ALeftView {
     interface ALeftViewImplUiBinder extends UiBinder<Widget, ALeftViewImpl> {
     }
 
+    @UiField
+    Frame a_frame;
+
     public ALeftViewImpl() {
         initWidget(uiBinder.createAndBindUi(this));
         testDiv("view:a");
+
+        a_frame.addLoadHandler(new LoadHandler() {
+
+            @Override
+            public void onLoad(final LoadEvent event) {
+                console("!! FRAME A has been loaded !!");
+            }
+        });
+
+        a_frame.setUrl("http://www.bing.com/");
     }
+
+    private native void console(String msg) /*-{
+        $wnd.console.log(msg);
+    }-*/;
 
     private native void testDiv(String title) /*-{
 
--- src/pgu/mvp/client/ui/left/BLeftViewImpl.java
+++ src/pgu/mvp/client/ui/left/BLeftViewImpl.java
@@ -3,8 +3,12 @@ package pgu.mvp.client.ui.left;
 import pgu.mvp.client.ui.BLeftView;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.LoadEvent;
+import com.google.gwt.event.dom.client.LoadHandler;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Frame;
 import com.google.gwt.user.client.ui.Widget;
 
 public class BLeftViewImpl extends Composite implements BLeftView {
@@ -14,10 +18,28 @@ public class BLeftViewImpl extends Composite implements BLeftView {
     interface BLeftViewImplUiBinder extends UiBinder<Widget, BLeftViewImpl> {
     }
 
+    @UiField
+    Frame b_frame;
+
     public BLeftViewImpl() {
         initWidget(uiBinder.createAndBindUi(this));
         testDiv("view:b");
+
+        b_frame.addLoadHandler(new LoadHandler() {
+
+            @Override
+            public void onLoad(final LoadEvent event) {
+                console("!! FRAME B has been loaded !!");
+            }
+        });
+
+        b_frame.setUrl("http://pgu-translate.appspot.com/");
+
     }
+
+    private native void console(String msg) /*-{
+        $wnd.console.log(msg);
+    }-*/;
 
     private native void testDiv(String title) /*-{
 
